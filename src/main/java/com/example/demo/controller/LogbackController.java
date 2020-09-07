734d5ee6dd9b5355dc37735ba95222304019cac3
huangys <1780392725@qq.com>
2020-09-07 16:03:08 +0800

    es

--- src/main/java/com/example/demo/controller/LogbackController.java
+++ src/main/java/com/example/demo/controller/LogbackController.java
@@ -1,30 +1,35 @@
 package com.example.demo.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author: faker
  * @DATE: 2020/8/11 16:26
  */
 @RestController
-
+@Slf4j
 public class LogbackController {
-    private static final Logger logger = LoggerFactory.getLogger(LogbackController.class);
+    //private static final Logger logger = LoggerFactory.getLogger(LogbackController.class);
 
     @RequestMapping("/test")
     public String test(){
-        logger.info("进入test方法");
-        logger.debug("进入test方法");
-        logger.warn("进入test方法");
-        logger.error("进入test方法");
         /*logger.info("进入test方法");
         logger.debug("进入test方法");
         logger.warn("进入test方法");
         logger.error("进入test方法");*/
+        log.info("进入test方法");
+        log.debug("进入test方法");
+        log.warn("进入test方法");
+        log.error("进入test方法");
+        String arr[]={"1","2"};
+        //arr.length;
         int a=1;
         int b=0;
         int c=a/b;
